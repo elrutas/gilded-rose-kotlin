a53e2bde26dfa9d52df42e88970f72862ae88e36
Lucas Lafarga <elrutas@gmail.com>
2017-09-29 16:25:24 +0200

    Invert if condition

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -38,7 +38,11 @@ class GildedRose(internal var items: Array<Item>) {
 
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
+                if (item.name == "Aged Brie") {
+                    if (item.quality < 50) {
+                        increaseQuality(item)
+                    }
+                } else {
                     if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (item.quality > 0) {
                             if (item.name != "Sulfuras, Hand of Ragnaros") {
@@ -47,10 +51,6 @@ class GildedRose(internal var items: Array<Item>) {
                         }
                     } else {
                         item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        increaseQuality(item)
                     }
                 }
             }

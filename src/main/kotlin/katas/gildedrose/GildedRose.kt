8c11872fe8030e39683987ef7a9db4ff79b3c64a
Lucas Lafarga <elrutas@gmail.com>
2017-09-29 16:29:35 +0200

    Extract two main methods

--- src/main/kotlin/katas/gildedrose/GildedRose.kt
+++ src/main/kotlin/katas/gildedrose/GildedRose.kt
@@ -9,46 +9,52 @@ class GildedRose(internal var items: Array<Item>) {
                 break
             }
 
-            if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality < 50) {
-                    increaseQuality(item)
+            updateQuality(item)
+            updateSellIn(item)
+        }
+    }
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                increaseQuality(item)
-                            }
-                        }
+    private fun updateQuality(item: Item) {
+        if (item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (item.quality < 50) {
+                increaseQuality(item)
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                increaseQuality(item)
-                            }
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            increaseQuality(item)
                         }
                     }
-                }
-            } else {
-                if (item.quality > 0) {
-                    decreaseQuality(item)
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            increaseQuality(item)
+                        }
+                    }
                 }
             }
+        } else {
+            if (item.quality > 0) {
+                decreaseQuality(item)
+            }
+        }
+    }
+
+    private fun updateSellIn(item: Item) {
+        item.sellIn = item.sellIn - 1
 
 
-            item.sellIn = item.sellIn - 1
-
-
-            if (item.sellIn < 0) {
-                if (item.name == "Aged Brie") {
-                    if (item.quality < 50) {
-                        increaseQuality(item)
-                    }
+        if (item.sellIn < 0) {
+            if (item.name == "Aged Brie") {
+                if (item.quality < 50) {
+                    increaseQuality(item)
+                }
+            } else {
+                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                    item.quality = item.quality - item.quality
                 } else {
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        item.quality = item.quality - item.quality
-                    } else {
-                        if (item.quality > 0) {
-                            decreaseQuality(item)
-                        }
+                    if (item.quality > 0) {
+                        decreaseQuality(item)
                     }
                 }
             }
